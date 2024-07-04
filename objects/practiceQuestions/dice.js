d3c495efa27a56f7242e34302e65b078f3a26098
prakash kumar pradhan <111282629+Prakash11122@users.noreply.github.com>
2024-07-04 17:35:38 +0530

    practiceQuestions

---
+++ objects/practiceQuestions/dice.js
@@ -0,0 +1,29 @@
+//Qs1. Create a program that generates a random number representing a dice roll. [The number should be between 1 and 6].
+
+// let random = Math.floor(Math.random()*6)+1;
+// console.log(`random number is ${random}`)
+
+
+//Qs2. Create an object representing a car that stores the following properties for the car: name, model, color.Print the car’s name.
+
+let car = {
+    carName : "Defender",
+    carModel : "Land Rover Discovery",
+    color : "White and Black"
+}
+
+console.log(car.carModel);
+
+
+//Qs3. Create an object Person with their name, age and city. Edit their city’s original value to change it to “New York”. Add a new property country and set it to the United States.
+
+
+let person = {
+    name : "Sundar",
+    age : 14,
+    city : "bhubaneswar"
+}
+
+console.log(person.city = "new York");
+
+
