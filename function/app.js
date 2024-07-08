f41ef06467e7cb91460d223bc481acb90b0b8ce2
prakash kumar pradhan <111282629+Prakash11122@users.noreply.github.com>
2024-07-08 18:17:54 +0530

    function

--- function/app.js
+++ function/app.js
@@ -31,11 +31,58 @@
 //return statement
 
 
-function sum(a,b){
-    console.log("prakash");
-    console.log("prakash");
-    return a+b;
-    console.log("prakash");
-    console.log("prakash");
+// function sum(a,b){
+//     console.log("prakash");
+//     console.log("prakash");
+//     return a+b;
+//     console.log("prakash");
+//     console.log("prakash");
+// }
+// sum(1,99);
+
+
+
+
+                               //scope
+
+//  1.function Scope ;       
+
+
+// let sum =5555;  // global scope
+// function calSum(a,b){
+//     let sum = a+b;   //function scope
+// console.log(sum);
+// }
+// calSum(5,5)
+// console.log(sum);
+
+
+
+//Block scope
+
+// {
+//     let a=10;
+// }
+// console.log(a)
+
+
+
+
+//lexical scope
+
+function outterScope(){
+    let x = 10;
+    let y = 20;
+
+    function innerFunction(){
+
+        let a = 11;
+
+        console.log(x);
+        console.log(y)
+
+    }
+
+    innerFunction();
 }
-sum(1,99);
+outterScope();
--- function/practiceQuation/practiceQ.js
+++ function/practiceQuation/practiceQ.js
@@ -57,3 +57,20 @@
 
 //Create a Function that returns the concatenation of all strings in an array
 
+
+let str = ["Hi" , "this" , "is" , "prakash"];
+
+function concat(str){
+
+
+    let result = "";
+
+    for(let i=0; i<str.length; i++){
+
+        result+=str[i]; 
+
+    }
+    return result;
+
+}
+
