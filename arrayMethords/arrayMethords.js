5615c861c3d396b3212421805c1ffa217476e1a8
prakash kumar pradhan <111282629+Prakash11122@users.noreply.github.com>
2024-07-10 23:00:54 +0530

    arrayMethord

---
+++ arrayMethords/arrayMethords.js
@@ -0,0 +1,142 @@
+//create a array, decclare objects firstname and marks in side array,  and print evry objects firstname using foreach method
+
+// let arr = [{
+//     firstName : "prakash",
+//     marks : 87
+// },
+// {
+//     firstName : "basu",
+//     marks : 82
+// },
+// {
+//     firstName : "devnath",
+//     marks : 89
+// }];
+
+// arr.forEach((student) => {
+//     console.log(student.firstName)
+// })
+
+
+
+//declare a array print each number using forEach method
+
+// let arr = [1,2,3,4,5];
+
+// let print = function(el){
+//     console.log(el);
+// }
+
+// arr.forEach(print);
+
+// let arr = [1,2,3,4,5];
+
+// arr.forEach(function(el){
+//     console.log(el)
+// })  
+
+
+// using arrow function print each array
+
+// let arr = [1,2,3,4,5];
+
+
+// arr.forEach((el) =>{
+//     console.log(el)
+// })
+
+
+
+
+
+
+
+// //map function
+// let arr = [2,4,6,8,10];
+
+// let square = arr.map((el) => {
+//     return el * el;
+// });
+
+
+
+
+
+
+                    //filter
+ 
+// let num = [1,2,4,6,8,10,3,5,7,12,45,66,44,99];
+
+// let even = num.filter((n) => {
+//     return n % 2 ==0;
+// });
+
+// let odd = num.filter((n) => {
+//     return n % 2 != 0;
+// });
+
+
+
+
+
+
+
+
+// reduce function
+ 
+
+
+
+
+
+// let arr = [1,2,3,4,5];
+
+// let finalValue = arr.reduce((acc , ele ) => {
+//     return acc + ele;
+
+// });
+
+// console.log(finalValue)
+
+
+
+
+
+// maximum value from an array
+
+// let array = [1,3,58,89];
+
+// let maximum = Math.max(...array);
+// console.log(maximum);
+
+
+
+// let array = [1,3,58,89];
+
+// let max= array.reduce((a,b) => Math.max(a,b));
+// console.log(max)
+
+
+
+
+let array = [1,3,58,89,99];
+
+let max = array.reduce((a,b) => {
+
+    if( b > a){
+        return b;
+    }else{
+        return a
+    }
+
+});
+
+console.log(max)
+
+
+
+
+                    
+
+
+
