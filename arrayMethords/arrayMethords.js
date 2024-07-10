b04d84a440ed9f27e17b3901406069996731f88c
prakash kumar pradhan <111282629+Prakash11122@users.noreply.github.com>
2024-07-10 23:07:29 +0530

    Create a function to find the min number in an array.

--- arrayMethords/arrayMethords.js
+++ arrayMethords/arrayMethords.js
@@ -104,6 +104,12 @@
 
 // maximum value from an array
 
+
+
+
+
+
+
 // let array = [1,3,58,89];
 
 // let maximum = Math.max(...array);
@@ -135,7 +141,18 @@ console.log(max)
 
 
 
+//Create a function to find the min number in an array.
 
+let arr = [1,3,5,6,8,99,77,55];
+
+let less = arr.reduce((min, max) => {
+    if(min < max){
+        return min;
+    }else{
+        return max
+    }
+});
+console.log(less)
                     
 
 
